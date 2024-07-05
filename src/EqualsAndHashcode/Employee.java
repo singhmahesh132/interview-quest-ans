850918c5f6bec969f123c89ba7de2d33a51151f6
Mahesh Singh <singh.mahesh132@gmail.com>
2024-07-05 19:44:38 +0530

    Initial commit

---
+++ src/EqualsAndHashcode/Employee.java
@@ -0,0 +1,40 @@
+package src.EqualsAndHashcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//Equals and hashcode without implementation
+public class Employee {
+    private String name;
+    private int age;
+
+    public Employee(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public static void main(String[] args){
+        Employee e = new Employee("nishi",29);
+        Employee e1 = new Employee("nishi",29);
+
+        System.out.println(e.hashCode());
+        System.out.println(e1.hashCode());
+        System.out.println(e.equals(e1));
+    }
+}
---
+++ src/FinalList/Main.java
@@ -0,0 +1,18 @@
+package src.FinalList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args){
+
+        final List<String> aList = new ArrayList<>();
+        aList.add("A");
+        aList.forEach(System.out::println);
+
+        //here array list variable cannot be assigned new object since it is final,
+        // will give compile error,
+        //but we can do all other list operations like add, remove etc.
+        //aList = new ArrayList<>();
+    }
+}
