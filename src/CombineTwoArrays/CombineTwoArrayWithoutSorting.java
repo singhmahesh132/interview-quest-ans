a3ec4208c53502f0b22d32160f50d1cd29e6c9c7
Mahesh Singh <singh.mahesh132@gmail.com>
2024-07-10 21:21:21 +0530

    Trimmed combine two sorted arrays to produce sorted array without sorting.

--- src/CombineTwoArrays/CombineTwoArrayWithoutSorting.java
+++ src/CombineTwoArrays/CombineTwoArrayWithoutSorting.java
@@ -5,41 +5,41 @@ public class CombineTwoArrayWithoutSorting {
     public static void main(String[] args){
         int[] arr1 = {13,20,45,51};
         int[] arr2 = {21,23,40,44,49,50,60,70,80};
+        int[] combArray = new int[arr1.length+arr2.length];
         int index1 = 0;
         int index2 = 0;
+        int combIndex = 0;
         boolean flag1 = true;
         boolean flag2 = true;
-        int index3 = 0;
-        int[] arr3 = new int[arr1.length+arr2.length];
 
         while(flag1 && flag2) {
 
             if (arr1[index1] < arr2[index2]) {
-                arr3[index3] = arr1[index1];
+                combArray[combIndex] = arr1[index1];
                 if (index1 < arr1.length - 1)
                     index1++;
                 else
                     flag1 = false;
             } else {
-                arr3[index3] = arr2[index2];
+                combArray[combIndex] = arr2[index2];
                 if (index2 < arr2.length - 1)
                     index2++;
                 else
                     flag2 = false;
             }
-            index3++;
+            combIndex++;
         }
 
         if(!flag1){
-            for(int i = index3; i < arr3.length; i++)
-                arr3[i] = arr2[index2++];
+            for(int i = combIndex; i < combArray.length; i++)
+                combArray[i] = arr2[index2++];
         }
         if(!flag2){
-            for(int i = index3; i < arr3.length; i++)
-                arr3[i] = arr1[index1++];
+            for(int i = combIndex; i < combArray.length; i++)
+                combArray[i] = arr1[index1++];
         }
 
-        for (int i : arr3) {
+        for (int i : combArray) {
             System.out.print(i + " ");
         }
     }
