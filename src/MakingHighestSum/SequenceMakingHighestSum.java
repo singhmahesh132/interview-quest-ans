181e8c168fc2900a1a99167910bd90a944122d4d
Mahesh Singh <159167042+singhmahesh132@users.noreply.github.com>
2024-07-10 14:39:58 +0530

    Update SequenceMakingHighestSum.java

--- src/MakingHighestSum/SequenceMakingHighestSum.java
+++ src/MakingHighestSum/SequenceMakingHighestSum.java
@@ -10,27 +10,25 @@ public class SequenceMakingHighestSum {
 
         int[] arr = {1,9,-10,5,3,-4,10,6,9,4,2,-9,2,-20,1,2,3};
 
+        int sum = arr[i];
+        int maxSum = arr[i];
         int startIndex = 0;
-        int sum = 0;
-        int maxSum = 0;
         int seqStartIndex = 0;
         int seqEndIndex = 0;
 
         //Start iteration over array
-        for(int i = 0 ; i < arr.length; i++){
+        for(int i = 1 ; i < arr.length; i++){
 
-            //if total sum becomes less than zero then reset the sum.
+            //if total sum becomes less than zero then reset the sum and also reset the startIndex
             if(sum < 0){
                 sum = 0;
+                startIndex = i;
             }
 
-            //when sum is 0 then reset both the index to point current index or else just increment the end index.
-            if(sum == 0)
-                startIndex = i;
             sum = sum + arr[i];
 
             //now compare the sum with seqObj sum and is less, then update the maxSum and start and end index.
-            if(maxSum < sum) {
+            if(maxSum <= sum) {
                 maxSum = sum;
                 seqStartIndex = startIndex;
                 seqEndIndex = i;
