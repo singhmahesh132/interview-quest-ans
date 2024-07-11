17e8660a5eb981d6fc7d11669d18bbc792a661b2
Mahesh Singh <159167042+singhmahesh132@users.noreply.github.com>
2024-07-11 16:54:35 +0530

    Added comments

--- src/ReplaceCharAndSortSentence/ReplaceAlpha.java
+++ src/ReplaceCharAndSortSentence/ReplaceAlpha.java
@@ -4,8 +4,11 @@ import java.util. Comparator;
 import java.til. HashMap; 
 import java.util.List;
 
+//Ques: Remove a character from string and sort the string on basis of length of words.
+//After sorting print the string with original words.
 public class ReplaceAlpha {
 
+//Manually created a method to remove character from each words, since string.replace() does not appends empty character.
 public static String removeAlphabetFronWord(String word, char alphaToRemove){
 
 	char[] wordArr = word. toCharArray();
@@ -27,6 +30,9 @@ public static void main(String[] args){
 	List<String> wordList = Arrays asList(word.split(“ “));
 	HashMap<String, String> myMap = new HashMap<>();
 
+//Using streams, first store the hashmap with replaced word as key and actual word as value.
+//then remove the character from words and sort then as per lenght
+//then using hashmap get the original words as value using modified word as key.
 	wordList.stream).map(letter -> {
 		myMap.put(ReplaceAlpha.removeAlphabetFromWord(letter,alpha),letter);
 		return ReplaceAlpha.removeAlphabetFromWord(letter,alpha);
