2d68cddd380a284c9a97021b6c0041fea33ac112
Mahesh Singh <159167042+singhmahesh132@users.noreply.github.com>
2024-07-23 18:42:32 +0530

    Update GreatestCommonDivisorString.java
    Added Working program for GCD of strings

--- src/LeetCodeEasy/GreatestCommonDivisorString.java
+++ src/LeetCodeEasy/GreatestCommonDivisorString.java
@@ -23,35 +23,31 @@ Input: str1 = "LEET", str2 = "CODE"
 Output: ""*/
 
 public class GreatestCommonDivisorString {
+    
     public static String gcdOfStrings(String str1, String str2) {
-        String[] wordToMatchArr = str2.split("");
-        Map<String, Integer> divCount = new HashMap<>();
-        StringBuilder wordToMatch = new StringBuilder();
-
-        for(int i = 0; i < wordToMatchArr.length; i++){
-            wordToMatch.append(wordToMatchArr[i]);
-            int count = matchSubString(str1, wordToMatch.toString());
-            if(count!=0)
-                divCount.put(wordToMatch.toString(),count);
+        for(int i = str2.length(); i > 0; i--){
+            String newWord = str2.substring(0,i);
+            if(getWordDivisor(str1, newWord)){
+                if(getWordDivisor(str2, newWord)){
+                    return newWord;
+                }
+            }
         }
-
-        if (divCount.isEmpty())
-            return "";
-        else
-            return divCount.entrySet().stream().min((o1, o2) -> o2.getValue().compareTo(o1.getValue())).get().getKey();
+        return "";
     }
 
-    public static int matchSubString(String str, String wrd){
+    public static boolean getWordDivisor(String str, String wrd){
         int divCount = 0;
-        while (!str.isEmpty()) {
-            if (str.startsWith(wrd)) {
+        while(!str.isEmpty()){
+            if(str.startsWith(wrd)){
                 divCount++;
                 str = str.substring(wrd.length());
-            } else
-                return 0;
+            }
+            else
+                return false;
         }
-        return divCount;
-    }
+        return true;
+    } 
 
     public static void main(String[] args){
         System.out.println(GreatestCommonDivisorString.gcdOfStrings("ABABAB","ABAB"));
